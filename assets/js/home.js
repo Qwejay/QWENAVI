4ccbeee63f45c432eaac6188d43c96635472e59a
QwejayHuang <dkm382638608@163.com>
2021-04-11 01:39:48 +0800

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,64 +2,11 @@
 
 // GitHub
 function updateGitHub(repository) {
-//    $('#github-watch img').attr('src', `https://img.shields.io/github/watchers/${repository}.svg?style=social&label=Watch`);
-//    $('#github-star img').attr('src', `https://img.shields.io/github/stars/${repository}.svg?style=social&label=Star`);
-//    $('#github-fork img').attr('src', `https://img.shields.io/github/forks/${repository}.svg?style=social&label=Fork`);
       $('#github-fork img').attr('src', `https://img.shields.io/badge/dynamic/json?label=%E5%BE%AE%E5%8D%9A%E5%85%B3%E6%B3%A8&query=%24.data.totalSubs&url=https%3A%2F%2Fapi.spencerwoo.com%2Fsubstats%2F%3Fsource%3Dweibo%26queryKey%3D1916069587&labelColor=e71f19&color=040000&logo=sina-weibo&longCache=true`);
 
 };
 var repository = $('meta[name=repository]').attr('content');
 updateGitHub(repository);
-setInterval(function () {
-    if (!document.hidden) {
-        updateGitHub(repository);
-    };
-}, 60000);
-
-// version
-dayjs.locale('zh-cn');
-dayjs.extend(window.dayjs_plugin_relativeTime);
-function updateVersion(timestamp) {
-    $('#version img').attr('src', ``);
-};
-var updateAt = $('meta[name=updated_at]').attr('content');
-updateVersion(updateAt);
-setInterval(function () {
-    if (!document.hidden) {
-        updateVersion(updateAt);
-    };
-}, 60000);
-
-// visit
-function updateVisit() {
-    $.getJSON(analyticsAPI.url, {
-        'module': 'API',
-        'method': 'VisitsSummary.getUniqueVisitors',
-        'idSite': analyticsAPI.id,
-        'period': 'day',
-        'date': 'today',
-        'format': 'JSON',
-        'token_auth': analyticsAPI.token
-    }, function (data) {
-        $('#today-visitors img').attr('src', `https://img.shields.io/badge/%E4%BB%8A%E6%97%A5%E8%AE%BF%E5%AE%A2-${encodeURIComponent(data.value)}-brightgreen.svg`);
-    });
-    $.getJSON(analyticsAPI.url, {
-        'module': 'API',
-        'method': 'Live.getCounters',
-        'idSite': analyticsAPI.id,
-        'lastMinutes': '30',
-        'format': 'JSON',
-        'token_auth': analyticsAPI.token
-    }, function (data) {
-        $('#live-visitors img').attr('src', `https://img.shields.io/badge/%E5%BD%93%E5%89%8D%E5%9C%A8%E7%BA%BF-${encodeURIComponent(data[0].visitors)}-brightgreen.svg`);
-    });
-};
-updateVisit();
-setInterval(function () {
-    if (!document.hidden) {
-        updateVisit();
-    };
-}, 60000);
 
 // search
 $('#search-services').dropdown();
