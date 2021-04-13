7e346204b69d3bdaedf38063a98e5244fd9119a7
QwejayHuang <dkm382638608@163.com>
2021-04-13 23:21:33 +0800

    Update hitokoto.js

--- assets/js/hitokoto.js
+++ assets/js/hitokoto.js
@@ -2,14 +2,14 @@
 
 $.getJSON('https://v1.hitokoto.cn', function (data) {
     $('#hitokoto').attr({
-        'href': 'hitokoto.cn?c=k&c=j',
+        'url': 'hitokoto.cn?c=k&c=j',
         'target': '_blank'
     }).append(data.hitokoto).append(
         $('<div>').addClass('detail').text(data.from)
     );
 }).fail(function () {
     $('#hitokoto').attr({
-        'href': 'https://i.qwejay.cn/',
+        'url': 'https://i.qwejay.cn/',
         'target': '_blank'
     }).html('<i class="lightbulb icon"></i>忘掉今天的人将被明天忘掉。<div class="detail">歌德</div>');
 });
